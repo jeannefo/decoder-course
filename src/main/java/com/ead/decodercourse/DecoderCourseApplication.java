95f334b3053dcb0d72341d892d3c95ec2ceb693b
jeannefo <jeannefoliveira@gmail.com>
2023-03-04 14:13:54 -0300

    criando o projeto

---
+++ src/main/java/com/ead/decodercourse/DecoderCourseApplication.java
@@ -0,0 +1,13 @@
+package com.ead.decodercourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecoderCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecoderCourseApplication.class, args);
+    }
+
+}
